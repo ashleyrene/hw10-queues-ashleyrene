3ec97867ca7d1c962444d6cdcf4657fb256edebf
ashleyrene <ashleyrashidian@gmail.com>
2025-03-30 18:17:19 -0400

    Create queue.c

---
+++ queue.c
@@ -0,0 +1,8 @@
+#include "queue.h"
+#include "tile_game.h"
+
+void enqueue(struct queue *q, struct game_state state) {}
+
+struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+
+int number_of_moves(struct game_state start) { return 0; }
